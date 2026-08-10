e9f92edf7a10a3cf5055ebf66cf5333cf04404ce
莱德斯诺 <Q663397592@outlook.com>
2026-08-11 07:32:50 +0800

    更新 friends.ts

--- XHBlogs/data/friends.ts
+++ XHBlogs/data/friends.ts
@@ -6,8 +6,16 @@ export const friendsData: Friend[] = [
     "id": "amiya",
     "name": "罗德岛 PRTS",
     "description": "记录泰拉大陆的各项数据与前文明遗迹，愿源石的阴霾早日散去。",
-    "avatar": "https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg",
+    "avatar": "⁠/friends/luode.webp⁠",
     "url": "https://prts.wiki/",
     "themeColor": "rgba(16, 185, 129, 0.5)"
   },
+  {
+    "id": "qingyu",
+    "name": "晴雨技术笔记",
+    "description": "一个有梦想的小站",
+    "avatar": "/friends/qingyu.webp",
+    "url": "https://www.onmyodev.com",
+    "themeColor": "rgba(59, 130, 246, 0.5)"
+  }
 ];
