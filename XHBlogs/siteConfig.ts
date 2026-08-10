ffcafe3f0ae7ed59f395cb5c57e81509d3951484
莱德斯诺 <Q663397592@outlook.com>
2026-08-11 03:02:50 +0800

    Update siteConfig.ts

--- XHBlogs/siteConfig.ts
+++ XHBlogs/siteConfig.ts
@@ -15,7 +15,7 @@ export const siteConfig = {
   navAfter: "博客",
 
   // 2. 头像设置 (支持网络链接，或将图片放入 public 文件夹后使用 "/me.jpg")
-  avatarUrl: "/images/avatar.jpg",
+  avatarUrl: "/images/avatar.webp",
 
   // 3. 网站背景设置 (二选一)
   // 如果想用纯图片背景，请在下面 bgImage 写路径，并将 useGradient 设为 false
