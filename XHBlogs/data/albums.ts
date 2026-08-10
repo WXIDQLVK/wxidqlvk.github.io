ebd1dc9997fa822421a4ea8ba23e50e57fd08c80
莱德斯诺 <Q663397592@outlook.com>
2026-08-11 05:28:13 +0800

    Update albums.ts

--- XHBlogs/data/albums.ts
+++ XHBlogs/data/albums.ts
@@ -7,7 +7,7 @@ export const albums: Album[] = [
     "id": "ripples-of-the-past",
     "title": "往昔的涟漪",
     "description": "流星划过夜空，生命的长河荡起涟漪",
-    "cover": "/xilian1.jpg",
+    "cover": "/photos/xilian1.webp",
     "date": "2026.08",
     "photos": [
       {
