a17e0414cdf92d1fb2ab530910a323bcebaa0104
莱德斯诺 <Q663397592@outlook.com>
2026-08-12 03:02:29 +0800

    Update friends.ts

--- XHBlogs/data/friends.ts
+++ XHBlogs/data/friends.ts
@@ -17,5 +17,37 @@ export const friendsData: Friend[] = [
     "avatar": "/friends/qingyu.webp",
     "url": "https://www.onmyodev.com",
     "themeColor": "rgba(59, 130, 246, 0.5)"
+  },
+  {
+    "id": "aloysama",
+    "name": "AloySama",
+    "description": "这里还很简单，简单到只有喜欢。",
+    "avatar": "https://bu.dusays.com/2026/05/24/6a129c1673b22.jpg",
+    "url": "https://www.aloysama.cloud",
+    "themeColor": "rgba(236, 72, 153, 0.5)"
+  },
+  {
+    "id": "dreamblog",
+    "name": "Dream Blog",
+    "description": "A personal space for code, science, and thoughts.",
+    "avatar": "https://pc-blog.github.io/next/bg/1.jpg",
+    "url": "https://pc-blog.github.io/next",
+    "themeColor": "rgba(99, 102, 241, 0.5)"
+  },
+  {
+    "id": "anze",
+    "name": "安泽の温馨小窝",
+    "description": "愿得一人心.",
+    "avatar": "https://anze.love/wp-content/uploads/2026/03/cropped-anze.jpg",
+    "url": "https://anze.love",
+    "themeColor": "rgba(245, 158, 11, 0.5)"
+  },
+  {
+    "id": "msydjt",
+    "name": "MSYDJT'Blog",
+    "description": "MSYDJT-门上一点就通的博客",
+    "avatar": "https://cnb.cool/MSYDJT/fuwari/-/git/raw/main/public/files/img/IMG_2651-768x769-1.png",
+    "url": "https://msydjt.com",
+    "themeColor": "rgba(14, 165, 233, 0.5)"
   }
 ];
