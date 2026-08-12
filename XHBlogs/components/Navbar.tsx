272421f19b128a09c14e8b0d31abd8a503f22746
莱德斯诺 <Q663397592@outlook.com>
2026-08-12 15:13:08 +0800

    Update Navbar.tsx

--- XHBlogs/components/Navbar.tsx
+++ XHBlogs/components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
     { name: '首页', href: '/' },
     { name: '项目', href: '/projects' },
     { name: '文章', href: '/timeline' },
-    { name: '杂谈', href: '/categories/notes' },
+    { name: '杂谈', href: '/chatter' },
     { name: '照片', href: '/photowall' },
     { name: '音乐', href: '/music' },
     { name: '说说', href: '/moments' },
